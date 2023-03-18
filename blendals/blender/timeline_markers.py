ef5ffe3d87c2daeb386e9ef816f7e97883d579a5
Dmytro Kostochko <alerion.um@gmail.com>
2023-03-18 09:45:29 +0100

    Add bar marker before video start

--- blendals/blender/timeline_markers.py
+++ blendals/blender/timeline_markers.py
@@ -8,7 +8,10 @@ def add_bars_markers(bars_number=10) -> None:
     clear_bars_markers()
 
     for i in range(bars_number):
-        current_bar = settings.SONG_START_BAR + i
+        current_bar = settings.SONG_START_BAR + i - 1
+        if current_bar <= 1:
+            current_bar = 1
+
         beat = (current_bar - 1) * settings.SONG_BAR_SIZE_BEATS
         frame = frame_calculator.beat_to_frame(beat)
         bpy.context.scene.timeline_markers.new(name=str(current_bar), frame=frame)
