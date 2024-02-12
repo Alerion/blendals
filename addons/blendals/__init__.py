838dd717285842e09d4997757b7498daebef44cb
Dmytro Kostochko <alerion.um@gmail.com>
2024-02-12 13:48:01 +0100

    Improve addon loading

--- addons/blendals/__init__.py
+++ addons/blendals/__init__.py
@@ -10,25 +10,55 @@ bl_info = {
     "category": "3D View",
 }
 
+import inspect
+import logging
 import sys
 import os.path
 from importlib import reload
 
+logger = logging.getLogger(__name__)
+
+# Inject external dependencies.
 DEPENDENCIES_PATH = os.path.join(os.path.dirname(__file__), '../dependencies')
 sys.path.insert(0, DEPENDENCIES_PATH)
 
-if "bpy" not in locals():
-    from . import parse_song
-else:
-    reload(parse_song)
+# Import modules to register.
+# It should contain `register_module` and `unregister_module` functions.
+import blendals.parse_song
 
-
-import bpy
+DO_MODULES_RELOAD = ("register" in locals())
 
 
 def register():
-    parse_song.register_module()
+    """
+    This function is executed by Blender when addon is loaded.
+    """
+    _register_addon_modules()
 
 
 def unregister():
-    parse_song.unregister_module()
+    """
+    This function is executed by Blender when addon is loaded.
+    """
+    _unregister_addon_modules()
+
+
+def _register_addon_modules():
+    for addon_module in _iterate_addon_modules():
+        if DO_MODULES_RELOAD:
+            reload(addon_module)
+
+        if hasattr(addon_module, "register_module"):
+            addon_module.register_module()
+
+
+def _unregister_addon_modules():
+    for addon_module in _iterate_addon_modules():
+        if hasattr(addon_module, "unregister_module"):
+            addon_module.register_module()
+
+
+def _iterate_addon_modules():
+    for variable_name, variable in globals().items():
+        if inspect.ismodule(variable) and variable.__package__.startswith("blendals"):
+            yield variable
